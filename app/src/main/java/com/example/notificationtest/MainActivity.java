1aa1da19f6d0624ed4c06db074e57f3890232580
LLLQQQ <1170494354@qq.com>
2017-01-26 19:27:56 +0800

    setSound(Uri soundUri)

--- app/src/main/java/com/example/notificationtest/MainActivity.java
+++ app/src/main/java/com/example/notificationtest/MainActivity.java
@@ -7,11 +7,14 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.drawable.Icon;
+import android.net.Uri;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import java.io.File;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
@@ -31,6 +34,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
             case R.id.send_notice:
                 Intent intent = new Intent(this, NotificationActivity.class);
                 PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+                Uri soundUri = Uri.fromFile(new File("/system/media/audio/alarms/Alarm_Buzzer.ogg"));
                 NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
                 Notification.Builder builder = new Notification.Builder(MainActivity.this);
                 builder.setSmallIcon(R.mipmap.ic_launcher)
@@ -39,6 +43,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
                         //          .setAutoCancel(true)
                         .setContentTitle("This is Joe's content title")
                         .setContentText("This is Joe's conient text")
+                        .setSound(soundUri)
                         .setContentIntent(pendingIntent);
                 manager.notify(1, builder.build());
                 break;
